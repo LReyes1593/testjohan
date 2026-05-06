6d7252b647ee16a6d21cc6cb57bc283770c969b0
LReyes1593 <luis.r.p@uinenlinea.mx>
2026-05-06 15:02:52 -0600

    Update web_app.py

--- web_app.py
+++ web_app.py
@@ -1,5 +1,4 @@
-# Pedir el nombre al usuario
-nombre = input("Hola, ¿cómo te llamas? ")
+def saludar():
+    print("Hola mundo")
 
-# Saludar usando un f-string (la forma más moderna y limpia)
-print(f"¡Un gusto saludarte, {nombre}! Espero que tengas un excelente día.")
+saludar()
